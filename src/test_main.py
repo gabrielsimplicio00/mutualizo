cd85974e0103387d46b6d59c6a8795dc92a20b65
gabrielsimplicio00 <gaasimplicio@gmail.com>
2023-03-17 15:51:57 -0300

    criado o endpoint que captura palavras em comum entre 2 frases

--- src/test_main.py
+++ src/test_main.py
@@ -6,3 +6,9 @@ def teste_reverse_integers():
 def teste_average_words_length():
     sentence = 'Hi all, my name is Tom...I am originally from Brazil.'
     assert get_average_words_length(sentence) == 3.55
+
+def teste_matched_mismatched_words():
+    sentence1 = "We are really pleased to meet you in our city"
+    sentence2 = "The city was hit by a really heavy storm"
+    result = [{'We', 'to', 'heavy', 'The', 'storm', 'meet', 'hit', 'pleased', 'are', 'by', 'a', 'in', 'was', 'you', 'our'}, {'really', 'city'}]
+    assert matched_mismatched_words(sentence1, sentence2) == result
