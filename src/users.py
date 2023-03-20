7a82c309be6b5b34b29b27156f43c5a3feee6123
gabrielsimplicio00 <gaasimplicio@gmail.com>
2023-03-20 11:42:05 -0300

    mais refatorações, rota index adicionada, legibilidade do código otimizada

--- src/users.py
+++ src/users.py
@@ -1,3 +1,14 @@
+from pydantic import BaseModel
+
+class User(BaseModel):
+    username: str
+    email:str | None = None
+    full_name: str | None = None
+    disabled: bool | None = None
+
+class UserInDB(User):
+    hashed_password: str
+
 fake_users_db = {
     "userteste": {
         "username": "userteste",
